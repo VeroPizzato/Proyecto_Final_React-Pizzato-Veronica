b6abbd496f77b6bda4825f23f1d492dea564cfb9
VeroPizzato <verizzato@gmail.com>
2023-10-03 13:48:26 -0300

    subo validaciones en formulario

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -3,6 +3,10 @@ import { CartContext } from "../context/CartContext";
 import { collection, getFirestore, addDoc, doc, updateDoc } from "firebase/firestore";
 import { Navigate } from "react-router-dom";
 
+const emailValido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
 const CheckOut = () => {
     const { cart, totalMonto, clear } = useContext(CartContext);
 
@@ -13,12 +17,81 @@ const CheckOut = () => {
 
     const generarOrden = () => {
 
+        const nombreDeUsuario = document.getElementById('usuario');  
+        const numeroDeTelefono = document.getElementById('tel');
+        const direccionEmail = document.getElementById('dire');
+
+        if (usuario.value === "") {  // nombre.length === 0
+            Swal.fire({
+                title: 'Por favor, ingrese su nombre.',                           
+                icon: 'warning',  
+                confirmButtonColor: '#d33',                               
+                confirmButtonText: 'Aceptar',
+            })           
+            usuario.focus();
+            return false;
+        }
+        else if (!/^[a-z A-Z]+$/.test(nombreDeUsuario.value)){
+            Swal.fire({
+                title: 'Por favor, ingrese un nombre válido.',                           
+                icon: 'warning',  
+                confirmButtonColor: '#d33',                               
+                confirmButtonText: 'Aceptar',
+            })        
+            usuario.focus();
+            return false;
+        }
+
+        if (tel.value === "") {  // telefono.length === 0
+            Swal.fire({
+                title: 'Por favor, ingrese un numero de telefono.',                           
+                icon: 'warning',  
+                confirmButtonColor: '#d33',                               
+                confirmButtonText: 'Aceptar',
+            })             
+            tel.focus();
+            return false;
+        }
+        else {
+            if (isNaN(numeroDeTelefono.value)){
+                Swal.fire({
+                    title: 'Por favor, ingrese un numero de telefono válido.',                           
+                    icon: 'warning',  
+                    confirmButtonColor: '#d33',                               
+                    confirmButtonText: 'Aceptar',
+                })                 
+                tel.focus();
+                return false;
+            }
+        }
+        
+        if (dire.value === "") {  // email.length === 0
+            Swal.fire({
+                title: 'Por favor, ingrese su correo electrónico.',                           
+                icon: 'warning',  
+                confirmButtonColor: '#d33',                               
+                confirmButtonText: 'Aceptar',
+            })              
+            dire.focus();
+            return false;
+        }
+        else if (!emailValido(direccionEmail.value)) {
+            Swal.fire({
+                title: 'Por favor, ingrese un correo electrónico válido.',                           
+                icon: 'warning',  
+                confirmButtonColor: '#d33',                               
+                confirmButtonText: 'Aceptar',
+            })              
+            dire.focus();
+            return false;
+        }
+
         const buyer = {name: nombre, phone: telefono, email: email};
         const productos = cart.map(item => ({id: item.id, title: item.nombre, quantity: item.quantity, price: item.precio}));  
         const fecha = new Date();
         const date = `${fecha.getFullYear()}-${fecha.getMonth() + 1}-${fecha.getDate()} ${fecha.getHours()}:${fecha.getMinutes()}`;
         const total = totalMonto();
-        const order = {buyer: buyer, productos: productos, date: date, total: total};
+        const order = {buyer: buyer, productos: productos, date: date, total: total};        
 
         // Insertar un documento en Firestore
         const db = getFirestore();
@@ -40,20 +113,19 @@ const CheckOut = () => {
                     <form>
                         <div>
                             <label for="nombre" className="form-label"><b>Nombre</b></label> 
-                            <input type="text" className="form-control my-2" placeholder="Nombre y Apellido" onInput={(e) => { setNombre(e.target.value) }} />
+                            <input type="text" required id="usuario" className="form-control my-2" placeholder="Nombre y Apellido" onInput={(e) => { setNombre(e.target.value) }} />
                         </div>
                         <div>
                             <label for="email" className="form-label"><b>E-mail</b> </label> 
-                            <input type="text" className="form-control my-2" placeholder="Email" onInput={(e) => { setEmail(e.target.value) }} />
+                            <input type="text" required id="dire" className="form-control my-2" placeholder="Email" onInput={(e) => { setEmail(e.target.value) }} />
                         </div>
                         <div>
                             <label for="telefono" className="form-label"><b>Teléfono</b> </label> 
-                            <input type="text" className="form-control my-2" placeholder="teléfono" onInput={(e) => { setTelefono(e.target.value) }} />
+                            <input type="text" required id="tel" className="form-control my-2" placeholder="teléfono" onInput={(e) => { setTelefono(e.target.value) }} />
                         </div>
                         <button type="button" className="btn btn-secondary btnCambioColor my-2" onClick={generarOrden}>Generar Orden</button>
                     </form>
-                </div>
-               
+                </div>               
             </div>
             <div className="row">
                 <div className="col-text-center">
